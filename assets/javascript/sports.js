3534f08152fd66aee1426f624b2230b612497645
Scott Reynolds <sreynolds@Landaa.local>
2018-01-20 11:21:07 -0800

    working sports new js

--- assets/javascript/sports.js
+++ assets/javascript/sports.js
@@ -1,78 +1,50 @@
-var eventfullAPI = '4JcXb6fXXRDWGvj9';
-var dateFormat = 'MM-DD-YYYY';
-var dateRange = 10;
-var eventData = [];
-
-// getEvents('city','state') // get events for city and state pair 
-// searchNews();
-
-pushToDiv();
+searchNews('Los Angeles');
 
 function searchNews(city,state) {
     var url = 'https://newsapi.org/v2/everything?' +
-        'domains=espn.com&' +
-        'from=2018-01-01&to=2018-01-30&' +
+        'domains=espn.com,si.com&' +
+        'from=2018-12-01' +
         'sortBy=popularity&' +
-        // 'country=us&' +
-        // 'sources=bleacher-report&' +
-        'q=football denver&' +
+        'q=' + 'teams ' + city + '&' +
         'apiKey=315c46529d14462bada6f13c7af58332';
 
     $.ajax({
         url: url,
         type: "GET",
     }).done(function (data) {
-        setTimeout(function(){
-            console.log(data);
-            pushToDiv(data.articles)
-        },100)
+        pushToDiv(data.articles)
     });
 }
 
-//HELLO
-
 function pushToDiv (data) {
-
-    // for (i=0; i < data.length; i++) {
-
-    // }
-    var tableImageSource = 'https://imagesource';
-    var tableURL = 'https://headlineurl';
-    var tableHeadline = 'Headline';
-    var tableSource = 'source.com';
-    var tableRow = $('<tr class="sports-news-row">');
-
-    var tableDataImage = $('<td class="sports-news-image">');
-    var tableDataHeadline = $('<td class="sports-news-headline">');
-    var tableDataSource = $('<td class="sports-news-source">');
-    var tableImage = $('<img>');
-    var tableLink = $('<a>');
-    var tableLink = document.createElement('a');
+    var dataLength = 5;
+    if (data.length < 5) {
+        dataLength = data.length;
+    }
+    for (i=0; i < dataLength; i++) {
+        var tableImageSource = data[i].urlToImage;
+        var tableURL = data[i].url;
+        var tableHeadline = data[i].title;
+        var tableSource = data[i].source.name;
+        var tableRow = $('<tr class="sports-news-row">');
     
-    $(tableImage).attr('src',tableImageSource);
-    $(tableDataImage).append(tableImage);
+        var tableDataImage = $('<td class="sports-news-image">');
+        var tableDataHeadline = $('<td class="sports-news-headline">');
+        var tableDataSource = $('<td class="sports-news-source">');
+        var tableImage = $('<img>');
+        var tableLink = $('<a>');
+        
+        tableImage.attr('src',tableImageSource);
+        tableDataImage.append(tableImage);
+        
+        tableLink.attr('href','tableURL');
+        tableLink.attr('target','_new');
+        tableLink.text(tableHeadline);
+        tableDataHeadline.append(tableLink);
     
-    $(tableLink).attr('href','tableURL');
-    $(tableLink).attr('target','_new');
-    $(tableLink).text(tableHeadline);
-    $(tableDataHeadline).append(tableLink);
-
-    $(tableDataSource).text(tableSource);
-
-    $(tableRow).append(tableDataImage,tableDataHeadline,tableDataSource);
-
-    $('#sports-div').append(tableRow);
+        tableDataSource.text(tableSource);
     
-
-    
-// document.write("<table border=1>")
-// for (row=1; row<=4; row++) {
-//    document.write("<tr>")
-//    for (col=1; col<=5; col++) {
-//          document.write("<td>R" + row + "<br>C" + col + "</td>")
-//     }
-//     document.write("</tr>")
-// }
-// document.write("</table>")
-
+        tableRow.append(tableDataImage,tableDataHeadline,tableDataSource);
+        $('#sports-div').append(tableRow.clone(true, true));
+    }
 }
